b2b64d2650e0d2bab1d1742b57a8fcb6018c9f86
josefjuna <juna.josef@gmail.com>
2024-02-23 15:53:00 +0100

    authentication controller implemented

---
+++ heroWars/src/main/java/com/exampleapp/heroWars/controller/AuthenticationController.java
@@ -0,0 +1,36 @@
+package com.exampleapp.heroWars.controller;
+
+import com.exampleapp.heroWars.model.AuthenticationResponse;
+import com.exampleapp.heroWars.model.User;
+import com.exampleapp.heroWars.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+private final AuthenticationService authenticationService;
+
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody User request
+    ){
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody User request
+    ){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+
+}
