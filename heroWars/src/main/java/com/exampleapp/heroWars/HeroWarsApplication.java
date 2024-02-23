438a5c75e1ac1eb68e130357db4627b7e1b8d560
josefjuna <juna.josef@gmail.com>
2024-02-23 13:29:06 +0100

    DB settings

---
+++ heroWars/src/main/java/com/exampleapp/heroWars/HeroWarsApplication.java
@@ -0,0 +1,13 @@
+package com.exampleapp.heroWars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroWarsApplication.class, args);
+	}
+
+}
---
+++ heroWars/src/test/java/com/exampleapp/heroWars/HeroWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleapp.heroWars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
